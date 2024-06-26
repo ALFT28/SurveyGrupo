099dc1f72cc878e881bd7c2e164a812702e531df
JOEL <JOEL@DESKTOP-HEVUQ5C>
2024-06-26 14:22:34 -0600

    #Se agrego codigo al SurveyDetailsView.Xaml.Cs

--- SurveyGrupo/SurveyDetailsView.xaml.cs
+++ SurveyGrupo/SurveyDetailsView.xaml.cs
@@ -2,8 +2,29 @@ namespace SurveyGrupo;
 
 public partial class SurveyDetailsView : ContentPage
 {
+	private readonly string[] teams =
+	{
+		"Alianza Lima",
+		"America",
+		"Boca Juniors",
+		"Caracas FC",
+		"colo-colo",
+		"Peñarol",
+		"Real Madrid",
+		"Saprissa"
+	};
+
 	public SurveyDetailsView()
 	{
 		InitializeComponent();
 	}
+
+    private async void FavoriteTeamButton_Clicked(object sender, EventArgs e)
+    {
+		var favoriteTeam = await DisplayActionSheet(Literals.FavoriteTeamTitle, null, null, teams);
+        if(!String.IsNullOrWhiteSpace(favoriteTeam))
+		{
+			FavoriteTeamLabel.Text = favoriteTeam;
+		}
+    }
 }
