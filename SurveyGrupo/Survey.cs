970058cfcc05dcde727c64eef8e197f2bcebef46
JOEL <JOEL@DESKTOP-HEVUQ5C>
2024-06-27 13:56:00 -0600

    #Se agrego la clase Survey

---
+++ SurveyGrupo/Survey.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SurveyGrupo
+{
+    internal class Survey
+    {
+
+        public string Name { get;set;}
+
+        public string Birthdate { get;set;}
+
+        public string FavoriteTeam { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} | {Birthdate} | {FavoriteTeam }";
+        }
+    }
+}
